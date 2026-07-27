948439f3709fe32756e63d91b8e74b0ec733c57b
Kiran Kumar Kandula <114183802+chinna0425@users.noreply.github.com>
2026-07-28 03:53:27 +0530

    Add main check to launch demo with server config

--- app.py
+++ app.py
@@ -199,4 +199,8 @@ with gr.Blocks(
         outputs=run_status
     )
 
-demo.launch()
+if __name__ == "__main__":
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
